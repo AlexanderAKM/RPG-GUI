b23d10d5ef3fd31591215a8688c3f365eead2dca
RobertHielkema <r.j.hielkema.1@student.rug.nl>
2023-06-01 16:43:16 +0200

    Added most of the things for the stats panel and a main in PlayerStatsPane to be able to test it which will later be deleted again.

--- src/main/java/nl/rug/ai/oop/rpg/view/players/PlayerStatsPane.java
+++ src/main/java/nl/rug/ai/oop/rpg/view/players/PlayerStatsPane.java
@@ -1,8 +1,10 @@
 package nl.rug.ai.oop.rpg.view.players;
 
 import nl.rug.ai.oop.rpg.model.players.Player;
+import nl.rug.ai.oop.rpg.model.players.students.AIStudent;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PlayerStatsPane extends JPanel {
 
@@ -11,12 +13,34 @@ public class PlayerStatsPane extends JPanel {
     JLabel social;
     JLabel wellbeing;
     JLabel money;
-    public PlayerStatsPane(Player player) {
+    String language;
+    public PlayerStatsPane(Player player, String language) {
         this.player = player;
+        this.language = language;
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        intelligence = new JLabel();
-        social = new JLabel();
-        wellbeing = new JLabel();
-        money = new JLabel();
+        if (this.language.equals("English")){
+            intelligence = new JLabel("Intelligence: " + this.player.getIntelligence());
+            social = new JLabel("Social: " + this.player.getSocial());
+            wellbeing = new JLabel("Wellbeing: " + this.player.getWellbeing());
+            money = new JLabel("Money: €" + this.player.getMoney());
+        } else if (this.language.equals("Dutch")){
+            intelligence = new JLabel("Intelligentie: " + this.player.getIntelligence());
+            social = new JLabel("Sociaal: " + this.player.getSocial());
+            wellbeing = new JLabel("Gezondheid: " + this.player.getWellbeing());
+            money = new JLabel("Geld: €" + this.player.getMoney());
+        }
+        this.add(intelligence);
+        this.add(social);
+        this.add(wellbeing);
+        this.add(money);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        PlayerStatsPane pane = new PlayerStatsPane(new AIStudent(), new String("English"));
+        frame.setSize(1000,800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(pane);
+        frame.setVisible(true);
     }
 }
