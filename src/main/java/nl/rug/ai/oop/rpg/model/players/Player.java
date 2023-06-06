a007a516bb9de37a9cd229d150a1829c16a501f2
RobertHielkema <r.j.hielkema.1@student.rug.nl>
2023-06-06 12:47:16 +0200

    Added a commented out version of the singleton implementation of Player if you guys would rather use that

--- src/main/java/nl/rug/ai/oop/rpg/model/players/Player.java
+++ src/main/java/nl/rug/ai/oop/rpg/model/players/Player.java
@@ -4,14 +4,13 @@ import nl.rug.ai.oop.rpg.model.inventory.*;
 import nl.rug.ai.oop.rpg.model.inventory.inventories.generalInventory;
 import nl.rug.ai.oop.rpg.model.location.*;
 
-public abstract class Player {
+public class Player {
 
     private int intelligence;
     private int social;
     private int energy;
     private int wellbeing;
     private int money;
-    //public LocationManager locationManager;
     private Room currentRoom;
     private generalInventory inventory;
 
@@ -21,8 +20,42 @@ public abstract class Player {
         this.energy = energy;
         this.wellbeing = wellbeing;
         this.money = money;
-        //this.currentRoom = locationManager.getPlayerRoom();
     }
+
+    /*private static Player player;
+
+    private Player() {
+        intelligence = 0;
+        social = 0;
+        wellbeing = 0;
+        money = 0;
+    }
+
+    public static Player getInstance(){
+        if (player == null) {
+            player = new Player();
+        }
+        return player;
+    }
+
+    public void chooseProgramme(String programme) {
+        if (programme.equals("AI") || programme.equals("Artificial Intelligence")) {
+            player.changeIntelligence(80);
+            player.changeSocial(60);
+            player.changeWellbeing(100);
+            player.changeMoney(0);
+        } else if (programme.equals("AP") || programme.equals("Applied Physics")) {
+            player.changeIntelligence(90);
+            player.changeSocial(50);
+            player.changeWellbeing(100);
+            player.changeMoney(0);
+        } else if (programme.equals("CS") || programme.equals("Computing Science")) {
+            player.changeIntelligence(100);
+            player.changeSocial(40);
+            player.changeWellbeing(100);
+            player.changeMoney(0);
+        }
+    }*/
 
     public void changeIntelligence(int change){
         this.intelligence += change;
