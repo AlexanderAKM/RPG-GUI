b04a7aaf07978b75a05a8863ba1b5b2463c1e5b1
Alexander <a.k.muller.2@student.rug.nl>
2023-06-08 13:12:17 +0200

    start of inventoryGUI

--- src/main/java/nl/rug/ai/oop/rpg/model/inventory/Inventory.java
+++ src/main/java/nl/rug/ai/oop/rpg/model/inventory/Inventory.java
@@ -1,8 +1,11 @@
 package nl.rug.ai.oop.rpg.model.inventory;
 
+import java.util.List;
+
 public interface Inventory {
     void addItem(Item item);
     void removeItem(Item item);
     boolean containsItem(Item item);
 
+    public List<Item> getItems();
 }
--- src/main/java/nl/rug/ai/oop/rpg/view/inventory/InventoryGUI.java
+++ src/main/java/nl/rug/ai/oop/rpg/view/inventory/InventoryGUI.java
@@ -1,72 +1,117 @@
 package nl.rug.ai.oop.rpg.view.inventory;
 
+import nl.rug.ai.oop.rpg.model.inventory.Inventory;
+import nl.rug.ai.oop.rpg.model.inventory.Item;
+import nl.rug.ai.oop.rpg.model.inventory.inventories.generalInventory;
+import nl.rug.ai.oop.rpg.model.inventory.inventories.generalInventory;
+import nl.rug.ai.oop.rpg.model.inventory.items.Alcohol;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import nl.rug.ai.oop.rpg.model.inventory.Inventory;
-import nl.rug.ai.oop.rpg.model.inventory.Item;
-import nl.rug.ai.oop.rpg.model.inventory.inventories.generalInventory;
-import nl.rug.ai.oop.rpg.model.inventory.items.Alcohol;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public class InventoryGUI extends JFrame {
+public class InventoryGUI extends JPanel {
+    public static final Map<String, BufferedImage> itemImages = new HashMap<>();
+    public static final String ITEM_IMAGE_PATH = "/resources/";
+    public static final int ITEM_WIDTH = 120;
+    public static final int ITEM_HEIGHT = 150;
+
     private Inventory inventory;
-    private DefaultListModel<Item> itemListModel;
-    private JList<Item> itemList;
+    private DefaultListModel<String> itemListModel;
+    private JList<String> itemList;
+
+    static {
+        loadItemImages();
+    }
 
     public InventoryGUI(Inventory inventory) {
         this.inventory = inventory;
 
-        // Create the main frame
-        setTitle("Inventory");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 400);
-        setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        // Create the item list and scroll pane
         itemListModel = new DefaultListModel<>();
         itemList = new JList<>(itemListModel);
         JScrollPane scrollPane = new JScrollPane(itemList);
         add(scrollPane, BorderLayout.CENTER);
 
-        // Create the button to add a new item
         JButton addItemButton = new JButton("Add Item");
         addItemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // This should be so that a new item is added to the inventory whenever that is supposed to happen, i'll fix later
-                String itemName = "Alcohol";  //JOptionPane.showInputDialog("Enter the item name:");
-                Item item;
-                item = new Alcohol(0, 0);
+                Item item = new Alcohol(0, 0);
+                String itemName = item.getName();
                 inventory.addItem(item);
-                itemListModel.addElement(item);
+                itemListModel.addElement(itemName);
             }
         });
         add(addItemButton, BorderLayout.SOUTH);
     }
 
-    private void addItemToInventory(Item item) {
-        inventory.addItem(item);
-        itemListModel.addElement(item);
-        itemList.revalidate();
-        itemList.repaint();
+    private static void loadItemImages() {
+        try {
+            itemImages.put("alcohol", ImageIO.read(InventoryGUI.class.getResource(ITEM_IMAGE_PATH + "alcohol.png")));
+            // Add more items as needed
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
-    private void removeItemFromInventory(Item item) {
-        inventory.removeItem(item);
-        itemListModel.removeElement(item);
-        itemList.revalidate();
-        itemList.repaint();
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i = 0; i < itemListModel.size(); i++) {
+            String itemName = itemListModel.get(i);
+            BufferedImage itemImage = itemImages.get(itemName.toLowerCase());
+            if (itemImage != null) {
+                int x = (getWidth() - ITEM_WIDTH) / 2;
+                int y = (getHeight() - ITEM_HEIGHT) / 2;
+                g.drawImage(itemImage, x, y, ITEM_WIDTH, ITEM_HEIGHT, null);
+            }
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(300, 400);
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+        itemListModel.clear();
+        for (Item item : inventory.getItems()) {
+            String itemName = item.getName();
+            itemListModel.addElement(itemName);
+        }
+        repaint();
     }
 
     public static void main(String[] args) {
-        Inventory inventory = new generalInventory();
-        SwingUtilities.invokeLater(new Runnable() { // Ensure that the GUI follows proper threading guidelines
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                InventoryGUI gui = new InventoryGUI(inventory);
-                gui.setVisible(true);
+                Inventory inventory = new generalInventory();
+                Item item = new Alcohol(0, 0);
+                inventory.addItem(item);
+
+                JFrame frame = new JFrame("Inventory");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                InventoryGUI inventoryGUI = new InventoryGUI(inventory);
+                frame.getContentPane().add(inventoryGUI);
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+
+                // Example of updating the inventory and view
+                inventory.addItem(new Alcohol(0, 0));
+                inventory.addItem(new Alcohol(0, 0));
+                inventoryGUI.setInventory(inventory);
             }
         });
     }
