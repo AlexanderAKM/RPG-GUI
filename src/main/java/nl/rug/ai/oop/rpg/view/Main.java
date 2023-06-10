326f195080447906baa16b8b4b1298a970326ba2
RobertHielkema <r.j.hielkema.1@student.rug.nl>
2023-06-10 15:34:34 +0200

    Added pictures for the Player stats and added a class and functions for saving and loading savefiles.

--- src/main/java/nl/rug/ai/oop/rpg/view/Main.java
+++ src/main/java/nl/rug/ai/oop/rpg/view/Main.java
@@ -4,11 +4,15 @@ import nl.rug.ai.oop.rpg.model.inventory.Inventory;
 import nl.rug.ai.oop.rpg.model.inventory.Item;
 import nl.rug.ai.oop.rpg.model.inventory.items.Alcohol;
 import nl.rug.ai.oop.rpg.model.players.Player;
+import nl.rug.ai.oop.rpg.model.players.SaveFiles;
 import nl.rug.ai.oop.rpg.view.inventory.InventoryGUI;
 import nl.rug.ai.oop.rpg.view.location.GamePanelGUI;
+import nl.rug.ai.oop.rpg.view.players.PlayerStatsPane;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     //Main class for the JFrame which should include everyone's panes
@@ -27,7 +31,35 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 1000);
         frame.add(new InventoryGUI(Player.getInstance().getInventory()), BorderLayout.EAST);
+        frame.add(new PlayerStatsPane(), BorderLayout.SOUTH);
         //frame.add(new GamePanelGUI(), BorderLayout.CENTER); // adds the game panel
         frame.setVisible(true);
     }
+
+    /**
+     * @author RobertHielkema
+     */
+    public void loadOrNewGame(JFrame frame){
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        JButton newGame = new JButton("New Game");
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //go to panel to create game with choice of language and player course
+            }
+        });
+        JButton loadGame = new JButton("Load from savefile");
+        loadGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //go to panel to load game
+            }
+        });
+        panel.add(newGame);
+        panel.add(loadGame);
+        frame.add(panel);
+        frame.revalidate();
+    }
+
 }
