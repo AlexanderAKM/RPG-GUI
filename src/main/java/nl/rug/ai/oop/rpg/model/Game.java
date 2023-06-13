aa84d75e17293add62899e9dd75997605b95bcb5
Alexander <a.k.muller.2@student.rug.nl>
2023-06-13 16:36:04 +0200

    Added Javadoc comments

--- src/main/java/nl/rug/ai/oop/rpg/model/Game.java
+++ src/main/java/nl/rug/ai/oop/rpg/model/Game.java
@@ -6,15 +6,30 @@ import nl.rug.ai.oop.rpg.view.Main;
 
 import javax.swing.*;
 
+/**
+ * This class represents the main game logic for the RPG.
+ * It controls the game flow, starting the game and transitioning between the beginning and main parts of the game.
+ *
+ * @author Alexander Müller
+ */
 public class Game {
 
     private Player player;
 
+    /**
+     * The main method that starts the game.
+     *
+     * @param args command line arguments, not used in this application
+     */
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
     }
 
+    /**
+     * Starts the game by showing the beginning part.
+     * When the beginning part is done, it continues to the main part of the game.
+     */
     public void start() {
         // Start with the Beginning view
         Beginning beginning = new Beginning(new Runnable() {
@@ -27,6 +42,10 @@ public class Game {
         beginning.show();
     }
 
+    /**
+     * Continues the game after the beginning part is done.
+     * It retrieves the player instance and starts the main part of the game.
+     */
     private void continueGame() {
         // After the beginning part is done, we should have a player instance now
         this.player = Player.getInstance();
