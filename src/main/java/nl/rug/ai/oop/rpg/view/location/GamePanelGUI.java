62ffd2fc05cdc1a91a9cd77dbfdb3d8e6e4a42f8
Alexander <a.k.muller.2@student.rug.nl>
2023-06-15 10:56:12 +0200

    Somehow it wasn't possible to search for items anymore. Fixed it.

--- src/main/java/nl/rug/ai/oop/rpg/view/location/GamePanelGUI.java
+++ src/main/java/nl/rug/ai/oop/rpg/view/location/GamePanelGUI.java
@@ -244,14 +244,13 @@ public class GamePanelGUI {
                 public void actionPerformed(ActionEvent e) {
                     if (itemListener != null) {
                         itemListener.onItemClicked(item);
+                        showGamePanel();
                     }
                 }
             });
             itemButton.setActionCommand("item");
             roomItemsPanel.add(itemButton);
         }
-        
-        roomItemsPanel.add(backButton);
         panel.revalidate();
         panel.repaint();
     }
