63d531b448d09fea17dd8ad9cc677f3652da6892
RobertHielkema <r.j.hielkema.1@student.rug.nl>
2023-06-15 12:58:40 +0200

    Worked more on making a better beginning screen and made the controller better

--- src/main/java/nl/rug/ai/oop/rpg/view/players/ChooseProgramme.java
+++ src/main/java/nl/rug/ai/oop/rpg/view/players/ChooseProgramme.java
@@ -3,6 +3,8 @@ package nl.rug.ai.oop.rpg.view.players;
 import nl.rug.ai.oop.rpg.controller.players.ChooseProgrammeController;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,11 +25,11 @@ public class ChooseProgramme extends JPanel {
     private int customSocial = 60;
     private String customProgrammeName;
     private ChooseProgrammeController listener;
+    GridBagConstraints c;
 
     public ChooseProgramme() {
-        listener = new ChooseProgrammeController();
         this.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        c = new GridBagConstraints();
         Explanation = new JLabel("<html>Choose the programme you want to follow<br/>(Choose wisely the programme you choose will influence the game)<html>", SwingConstants.CENTER);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.ipady = 100;
@@ -39,66 +41,40 @@ public class ChooseProgramme extends JPanel {
         c.gridy = 0;
         this.add(Explanation, c);
 
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new GridLayout(2,4, 5, 0));
+
         AIButton = new JButton("Artificial Intelligence");
-        AIButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                listener.chosenProgramme("Artificial Intelligence");
-            }
-        });
-        c.gridwidth = 1;
-        c.gridy = 1;
-        this.add(AIButton, c);
+        AIButton.setActionCommand("Artificial Intelligence");
+        AIButton.addActionListener(new ChooseProgrammeController(this));
+        buttons.add(AIButton);
 
         APButton = new JButton("Applied Physics");
-        APButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                listener.chosenProgramme("Applied Physics");
-            }
-        });
-        c.gridx = 1;
-        this.add(APButton, c);
+        APButton.setActionCommand("Applied Physics");
+        APButton.addActionListener(new ChooseProgrammeController(this));
+        buttons.add(APButton);
 
         CSButton = new JButton("Computing Science");
-        CSButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                listener.chosenProgramme("Computing Science");
-            }
-        });
-        c.gridx = 2;
-        this.add(CSButton, c);
+        CSButton.setActionCommand("Computing Science");
+        CSButton.addActionListener(new ChooseProgrammeController(this));
+        buttons.add(CSButton);
 
         CustomButton = new JButton("Custom");
-        CustomButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                createCustomProgramme();
-            }
-        });
-        c.gridx = 3;
-        this.add(CustomButton, c);
+        CustomButton.setActionCommand("Custom");
+        CustomButton.addActionListener(new ChooseProgrammeController(this));
+        buttons.add(CustomButton);
 
         AILabel = new JLabel("<html>Intelligence: 70<br/>Social: 60<html>", SwingConstants.CENTER);
-        c.gridy = 2;
-        c.gridx = 0;
-        this.add(AILabel, c);
+        buttons.add(AILabel);
 
         APLabel = new JLabel("<html>Intelligence: 85<br/>Social: 40<html>", SwingConstants.CENTER);
-        c.gridx = 1;
-        this.add(APLabel, c);
+        buttons.add(APLabel);
 
         CSLabel = new JLabel("<html>Intelligence: 100<br/>Social: 20<html>", SwingConstants.CENTER);
-        c.gridx = 2;
-        this.add(CSLabel, c);
+        buttons.add(CSLabel);
 
-        customLabel = new JLabel("<html>Choose the stats for your custom programme<html>");
-        c.gridx = 3;
-        this.add(customLabel, c);
-
-        customSlider = new JSlider(JSlider.HORIZONTAL, 10, 120, customIntelligence);
-        this.add(customSlider, c);
+        c.gridy = 1;
+        this.add(buttons, c);
     }
 
     public void createCustomProgramme(){
@@ -106,9 +82,54 @@ public class ChooseProgramme extends JPanel {
         for (Component component : components){
             remove(component);
         }
+        this.setLayout(new GridLayout(3,3));
         this.revalidate();
+        this.repaint();
+        this.setLayout(new GridBagLayout());
+
+        Explanation = new JLabel("<html>Choose the stats for your custom programme<html>", SwingConstants.CENTER);
+        c.weightx = 0.5;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.gridx = 1;
+        c.gridy = 3;
+        this.add(Explanation, c);
+
+        customLabel = new JLabel("<html>Intelligence: <html>" + customIntelligence + "<html><br/>Social: <html>" + customSocial, SwingConstants.CENTER);
+        c.gridy = 5;
+        this.add(customLabel, c);
+
+        customSlider = new JSlider(JSlider.HORIZONTAL, 10, 120, customIntelligence);
+        customSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                customIntelligence = ((JSlider)e.getSource()).getValue();
+                customSocial = 150-customIntelligence;
+                customLabel.setText("<html>Intelligence: <html>" + customIntelligence + "<html><br/>Social: <html>" + customSocial);
+            }
+        });
+        customSlider.setPaintLabels(true);
+        customSlider.setPaintTrack(true);
+        customSlider.setPaintTicks(true);
+        customSlider.setMajorTickSpacing(30);
+        customSlider.setMinorTickSpacing(10);
+        c.gridy = 4;
+        c.gridx = 1;
+        this.add(customSlider, c);
 
 
+
+    }
+
+    public int getCustomIntelligence() {
+        return customIntelligence;
+    }
+
+    public int getCustomSocial() {
+        return customSocial;
+    }
+
+    public String getCustomProgrammeName() {
+        return customProgrammeName;
     }
 
     public static void main(String[] args) {
