e9d1f192050fb5f8dcc078a9a1b4851960f0dc28
RobertHielkema <r.j.hielkema.1@student.rug.nl>
2023-06-17 16:53:07 +0200

    Added javadoc to PlayerStatsPane

--- src/main/java/nl/rug/ai/oop/rpg/view/players/PlayerStatsPane.java
+++ src/main/java/nl/rug/ai/oop/rpg/view/players/PlayerStatsPane.java
@@ -8,6 +8,9 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
+ * The PlayerStatsPane class represents a panel that displays the player's statistics.
+ * It extends JPanel and implements PropertyChangeListener.
+ *
  * @author RobertHielkema
  */
 public class PlayerStatsPane extends JPanel implements PropertyChangeListener {
@@ -23,18 +26,28 @@ public class PlayerStatsPane extends JPanel implements PropertyChangeListener {
     JLabel money;
     final int STATHEIGHT = 80;
     final int STATWIDTH = 80;
+
+    /**
+     * Constructs a new PlayerStatsPane object.
+     * Initializes the player, adds itself as a change listener to the player,
+     * sets the layout, creates and initializes the stat labels and icons,
+     * and adds them to the panel.
+     */
     public PlayerStatsPane() {
         this.player = Player.getInstance();
         player.addChangeListener(this);
         this.setLayout(new GridLayout(2,4, 5, 0));
+
         intelligenceIcon = new JLabel(resizeImage(new ImageIcon(PlayerStatsPane.class.getResource("/intelligence_stat.jpg"))));
         socialIcon = new JLabel(resizeImage(new ImageIcon(PlayerStatsPane.class.getResource("/social_stat.jpg"))));
         wellbeingIcon = new JLabel(resizeImage(new ImageIcon(PlayerStatsPane.class.getResource("/wellbeing_stat.png"))));
         moneyIcon = new JLabel(resizeImage(new ImageIcon(PlayerStatsPane.class.getResource("/money_stat.png"))));
+
         intelligence = new JLabel(String.valueOf(this.player.getIntelligence()), SwingConstants.CENTER);
         social = new JLabel(String.valueOf(this.player.getSocial()), SwingConstants.CENTER);
         wellbeing = new JLabel(String.valueOf(this.player.getWellbeing()), SwingConstants.CENTER);
         money = new JLabel(String.valueOf(this.player.getMoney()), SwingConstants.CENTER);
+
         this.add(intelligenceIcon);
         this.add(socialIcon);
         this.add(wellbeingIcon);
@@ -43,15 +56,26 @@ public class PlayerStatsPane extends JPanel implements PropertyChangeListener {
         this.add(social);
         this.add(wellbeing);
         this.add(money);
+
         this.setVisible(true);
     }
 
+    /**
+     * Resizes the given ImageIcon to the specified dimensions.
+     *
+     * @param imageIcon the ImageIcon to resize
+     * @return the resized ImageIcon
+     */
     private ImageIcon resizeImage(ImageIcon imageIcon){
-        Image image = imageIcon.getImage(); // transform it
-        Image scaledImage = image.getScaledInstance(STATWIDTH, STATHEIGHT,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        Image image = imageIcon.getImage();
+        Image scaledImage = image.getScaledInstance(STATWIDTH, STATHEIGHT,  java.awt.Image.SCALE_SMOOTH);
         return new ImageIcon(scaledImage);
     }
 
+    /**
+     * Invalidates the panel.
+     * Updates the stat labels with the player's current statistics and repaints the panel.
+     */
     @Override
     public void invalidate() {
         super.invalidate();
@@ -62,15 +86,12 @@ public class PlayerStatsPane extends JPanel implements PropertyChangeListener {
         this.repaint();
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        PlayerStatsPane pane = new PlayerStatsPane();
-        frame.setSize(1000,800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(pane, BorderLayout.SOUTH);
-        frame.setVisible(true);
-    }
-
+    /**
+     * Listens for property change events.
+     * If the property name is "playerstat", revalidates the panel.
+     *
+     * @param evt the PropertyChangeEvent object
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if(evt.getPropertyName().equals("playerstat")) {
